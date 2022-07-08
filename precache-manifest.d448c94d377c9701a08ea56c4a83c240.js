867072f541065f61f486c2414fe26cd00627ff45
vedant jawanjal <66886329+chotudevil@users.noreply.github.com>
2022-07-08 19:38:27 +0530

    Updates

---
+++ precache-manifest.d448c94d377c9701a08ea56c4a83c240.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "950e993abc129667d0f7",
+    "url": "/TextUtils/static/css/main.f04b8fe7.chunk.css"
+  },
+  {
+    "revision": "950e993abc129667d0f7",
+    "url": "/TextUtils/static/js/main.950e993a.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/TextUtils/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/TextUtils/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "14506651abc0b1064f2d",
+    "url": "/TextUtils/static/js/runtime~main.14506651.js"
+  },
+  {
+    "revision": "3cf01fa8b35b8f16b00f0c88a3843b86",
+    "url": "/TextUtils/index.html"
+  }
+];
